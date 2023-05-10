104500e331224be3e44d40eb44229e4f3679a26c
Benjamin Geets <bge@tuta.io>
2023-05-10 20:34:42 +0200

    config: exclude swiper from compiler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css:['~/assets/css/reset.css', '~/assets/css/typo.css']
+  css:['~/assets/css/reset.css', '~/assets/css/typo.css'],
+  vue:{
+    compilerOptions:{
+      isCustomElement: (tag) => ['swiper-container', 'swiper-slide'].includes(tag),
+    }
+  }
 })
